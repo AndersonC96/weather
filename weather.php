a7f812b566fb84670e35a22e3f4b247e9cbc9ce3
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-01 17:20:45 -0300

    Create weather.php

---
+++ weather.php
@@ -0,0 +1,12 @@
+<?php
+    $config = include('./weather/api.php');
+    $apiKey = $config['api_key'];
+    if(isset($_GET['city'])){
+        $city = urlencode($_GET['city']);
+        $apiUrl = "http://api.weatherapi.com/v1/current.json?key={$apiKey}&q={$city}&aqi=no";
+        $weatherData = file_get_contents($apiUrl);
+        echo $weatherData;
+    }else{
+        echo json_encode(['error' => 'No city provided']);
+    }
+?>
